64ec977da1131338f16354a87a66e855d97adcaf
PedroFiorio <94929673+PedroFiorio@users.noreply.github.com>
2025-06-03 20:42:27 -0300

    update: history activity

--- app/src/main/java/br/com/ufv/inf311/solicitaedu/HistoryActivity.java
+++ app/src/main/java/br/com/ufv/inf311/solicitaedu/HistoryActivity.java
@@ -3,10 +3,16 @@ package br.com.ufv.inf311.solicitaedu;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class HistoryActivity extends Activity {
+
+    LinearLayout cardContainer;
+    LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,26 @@ public class HistoryActivity extends Activity {
         setContentView(R.layout.activity_history);
 
         ActivityBuilder.build(this, "Histórico");
+
+        cardContainer = findViewById(R.id.history_card_container);
+        inflater = LayoutInflater.from(this);
+        for (int i = 10; i > 0; i--) {
+            View card;
+            if (i % 3 == 0) {
+                card = inflater.inflate(R.layout.concluded_history_card, cardContainer, false);
+                TextView title = card.findViewById(R.id.concluded_history_card_title);
+                title.setText("Solicitação #" + i);
+                TextView date = card.findViewById(R.id.concluded_history_card_date);
+                date.setText("03/06/2025");
+            } else {
+                card = inflater.inflate(R.layout.waiting_history_card, cardContainer, false);
+                TextView title = card.findViewById(R.id.waiting_history_card_title);
+                title.setText("Solicitação #" + i);
+                TextView date = card.findViewById(R.id.waiting_history_card_date);
+                date.setText("03/06/2025");
+            }
+            cardContainer.addView(card);
+        }
     }
 
     // Go to Home (MainActivity).
@@ -23,9 +49,7 @@ public class HistoryActivity extends Activity {
     }
 
     // Do nothing, since it's the current Activity.
-    public void openHistoryActivity(View v) {
-
-    }
+    public void openHistoryActivity(View v) { }
 
     // Go to History Activity.
     public void openRequestActivity(View v) {
