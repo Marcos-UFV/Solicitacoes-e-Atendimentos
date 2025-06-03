b9b5c0074e98797c70ba05ebbab7f2f127ad8595
alfeijo28 <andre.santos1@ufv.br>
2025-06-03 17:49:18 -0300

    style: adding button icons

--- app/src/main/java/br/com/ufv/inf311/solicitaedu/MainActivity.java
+++ app/src/main/java/br/com/ufv/inf311/solicitaedu/MainActivity.java
@@ -1,10 +1,14 @@
 package br.com.ufv.inf311.solicitaedu;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends Activity {
     @Override
@@ -15,6 +19,22 @@ public class MainActivity extends Activity {
         ActivityBuilder.build(this, "Home");
     }
 
+    // === HOME BUTTONS ===
+    // Opens university website.
+    public void openUniversityWebsite(View v) {
+        Uri uri = Uri.parse("https://www.ufv.br/");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+
+    // Calls university support telephone.
+    public void callUniversity(View v) {
+        Uri uri = Uri.parse("tel:+55 (31) 3612-1081");
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+
+    // === FOOTER BUTTONS ===
     // Do nothing, since it's the current Activity.
     public void openHomeActivity(View v) {
 
