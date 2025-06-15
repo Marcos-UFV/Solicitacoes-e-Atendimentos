1de7aa7539a51de5c00b9cae35dcc86447e8ed17
Marcos <aurelio.marcos@ufv.br>
2025-06-15 20:28:23 -0300

    add: endpoints interface and client implemetation for retrofit

--- app/src/main/java/br/com/ufv/inf311/solicitaedu/network/ApiClient.java
+++ app/src/main/java/br/com/ufv/inf311/solicitaedu/network/ApiClient.java
@@ -1,4 +1,18 @@
 package br.com.ufv.inf311.solicitaedu.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
+    private static final String BASE_URL = "https://crmufvgrupo4.apprubeus.com.br";
+    private static Retrofit retrofit = null;
+    public static Retrofit getClientRx() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
--- app/src/main/java/br/com/ufv/inf311/solicitaedu/network/RubeusEndpointsAPI.java
+++ app/src/main/java/br/com/ufv/inf311/solicitaedu/network/RubeusEndpointsAPI.java
@@ -1,4 +1,14 @@
 package br.com.ufv.inf311.solicitaedu.network;
 
+import br.com.ufv.inf311.solicitaedu.model.Contact;
+import br.com.ufv.inf311.solicitaedu.model.ContactDTO;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface RubeusEndpointsAPI {
+    @POST("/api/Contato/dadosPessoas")
+    Call<ContactDTO> getContact(@Body Contact contact, @Query("origem") String origem, @Query("token") String token);
 }
