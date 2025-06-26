c3d950835a882a5d243c547095b0c735a43eaf8a
Marcos <aurelio.marcos@ufv.br>
2025-06-26 13:15:13 -0300

    Add: Request, RequestDTO and RequestType classes

--- app/src/main/java/br/com/ufv/inf311/solicitaedu/model/Request.java
+++ app/src/main/java/br/com/ufv/inf311/solicitaedu/model/Request.java
@@ -1,4 +1,43 @@
 package br.com.ufv.inf311.solicitaedu.model;
 
-public class Request {
+
+import java.io.Serializable;
+
+public class Request implements Serializable {
+    private int tipo;
+    private PersonField pessoa;
+    private CustomField camposPersonalizados;
+    public Request(int tipo, Contact pessoa,String description) {
+        this.tipo = tipo;
+        this.pessoa = new PersonField(pessoa.getId());
+        this.camposPersonalizados = new CustomField(description);
+    }
+    private static class PersonField{
+        int id;
+        public PersonField(int id){
+            this.id = id;
+        }
+    }
+    private static class CustomField {
+        public CustomField(String description){
+            this.campopersonalizado_6_compl_proc = description;
+        }
+        String campopersonalizado_6_compl_proc;
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "campopersonalizado_6_compl_proc='" + campopersonalizado_6_compl_proc + '\'' +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "tipo=" + tipo +
+                ", pessoa=" + pessoa +
+                ", camposPersonalizados=" + camposPersonalizados +
+                '}';
+    }
 }
--- app/src/main/java/br/com/ufv/inf311/solicitaedu/model/RequestType.java
+++ app/src/main/java/br/com/ufv/inf311/solicitaedu/model/RequestType.java
@@ -1,4 +1,25 @@
 package br.com.ufv.inf311.solicitaedu.model;
 
 public enum RequestType {
+    APROVEITAMENTO_DISICPLINA(111,"Solicitação de Aproveitamento de Disciplina"),
+    HISTORICO_ESCOLAR(0,"Histórico Escolar Graduação"),
+    CERTIDAO_DE_MATRICULA(0,"Certidão de Matrícula"),
+    CERTIDAO_DE_VINCULO_ESTUDANTIL(0,"Certidão de que é aluno"),
+    SOLICITACAO_EXAME_SUFICIENCIA(0,"Solicitação de Exame de Suficiência"),
+    ALTERACAO_PRE_OU_CO_REQUISITO(0,"Alteração de Pré(co)-Requisito"),
+    TRANCAMENTO_MATRICULA(0,"Trancamento de Matrícula");
+    private int id;
+    private String description;
+
+    RequestType(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
